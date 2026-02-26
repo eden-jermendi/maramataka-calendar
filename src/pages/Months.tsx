d022ff95a3a61f79d300ad49536e1af8881470f2
Eden Jermendi <ejermendi@gmail.com>
2026-02-26 15:47:41 +1300

    Removed legend and left note to add later as redunant

--- src/pages/Months.tsx
+++ src/pages/Months.tsx
@@ -15,11 +15,6 @@ const Month: React.FC = () => {
     )
   }
 
-  const legendDays = month.days
-    .map((id) => lunarDaysById[id])
-    .filter((day): day is NonNullable<typeof day> => Boolean(day))
-    .sort((a, b) => a.order - b.order)
-
   return (
     <div className="MonthPage">
       <h1>{month.nameTr}</h1>
@@ -43,13 +38,7 @@ const Month: React.FC = () => {
           )
         })}
       </ul>
-
-      <h2>Legend</h2>
-      <ol>
-        {legendDays.map((day) => (
-          <li key={day.id}>{day.nameTr}</li>
-        ))}
-      </ol>
+      {/* // TODO: add legend when month grid/calendar view is introduced */}
     </div>
   )
 }
